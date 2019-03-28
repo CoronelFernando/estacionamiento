e8f511b4189268f08d18b6560ce96f5ca6e35cf9
Mirna Valdivia <miirna.valdiiviia@gmail.com>
2019-03-28 14:59:08 -0700

    resolviendo conflictos

--- resources/views/pagesweb/administrador/principal.blade.php
+++ resources/views/pagesweb/administrador/principal.blade.php
@@ -28,37 +28,37 @@
     </div>
   </form>
   <!--modal Reservacion-->
-  <div class="modal" tabindex="-1" role="dialog">
-    <div class="modal-dialog" role="document">
+  
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+      <!-- Modal content-->
       <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Reservacion</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>                
-      </div>
-      <div class="modal-body">
-        <form>
-          <div class="form-group">
-            <label>Usuario:</label>
-            <input class="form-control">                
-          </div>
-          <div class="form-group">
-            <label>No. Cajon:</label>
-            <input>
-          </div>
-          <div class="form-group">
-            <label>Tiempo:</label>
-            <input class="form-control">                
-          </div>
-          </form>
+        <div class="modal-header">
+          <h4 class="modal-title">Modal Header</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>          
+        </div>
+        <div class="modal-body">
+          <form>
+          	<div class="form-group">
+            	<label>Usuario:</label>
+            	<input class="form-control">                
+          	</div>
+            <div class="form-group">
+              <label>No. Cajon:</label>
+              <input class="form-control">
+           </div>
+            <div class="form-group">
+              <label>Tiempo:</label>
+              <input class="form-control">                
             </div>
-            <div class="modal-footer">
-        <button type="button" class="btn btn-primary">Aceptar</button>          
-        </div>
-        </div>
-      </div>        
+          </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+      </div>
     </div>
-  </form>
+  </div>
+
  
 @endsection
